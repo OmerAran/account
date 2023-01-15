f9a58beec734b2271ab6d92cfd160da71126677d
OmerAran <omerr.aran@gmail.com>
2023-01-15 15:15:32 +0300

    controllers added for account and customer

--- src/main/java/com/omeraran/account/controller/AccountController.java
+++ src/main/java/com/omeraran/account/controller/AccountController.java
@@ -1,4 +1,28 @@
 package com.omeraran.account.controller;
 
+import com.omeraran.account.dto.AccountDto;
+import com.omeraran.account.dto.CreateAccountRequest;
+import com.omeraran.account.service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("v1/account")
 public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountDto> createAccount(@Valid @RequestBody CreateAccountRequest createAccountRequest){
+        return ResponseEntity.ok(accountService.createAccount(createAccountRequest));
+    }
 }
