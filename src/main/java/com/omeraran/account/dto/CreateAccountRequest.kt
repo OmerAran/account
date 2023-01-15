b360d9cb69eae3a0099b3a4fe7c7ad6f5f495f22
OmerAran <omerr.aran@gmail.com>
2023-01-15 15:14:03 +0300

    request model is added

---
+++ src/main/java/com/omeraran/account/dto/CreateAccountRequest.kt
@@ -0,0 +1,13 @@
+package com.omeraran.account.dto
+
+import java.math.BigDecimal
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+
+data class CreateAccountRequest(
+    @field:NotBlank
+    val customerId : String?,
+    @field:Min(0)
+    val initialCredit : BigDecimal?
+) {
+}
